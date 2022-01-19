3e0f9f28f6de2bcf2c8b8b75bb8e8aed8fbfc190
Rishith Kyatham <rkyatham01@gmail.com>
2022-01-19 01:05:30 -0500

    first assignment

---
+++ poem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    printf(" Hold fast to dreams \n");
+    printf(" For if dreams die \n");
+    printf(" Life is a broken-winged bird \n");
+    printf(" That cannot fly. \n");
+    printf(" \n");
+    printf(" Hold fast to dreams \n");
+    printf(" For when dreams go \n");
+    printf(" Life is a barren field \n");
+    printf(" Frozen with snow. \n");
+
+    return 0;
+}
